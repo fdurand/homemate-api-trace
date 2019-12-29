e125fab4400157238a5b943a4186cac8886aab25
Durand Fabrice <oeufdure@gmail.com>
2019-12-28 22:14:59 -0500

    First draft of homemate client

--- aes.go
+++ aes.go
@@ -3,23 +3,32 @@ package main
 import (
 	"bytes"
 	"crypto/aes"
-	"crypto/cipher"
 	"fmt"
 )
 
-func AESEncrypt(src string, key []byte) []byte {
+func AESEncrypt(content []byte, key []byte) []byte {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		fmt.Println("key error1", err)
 	}
-	if src == "" {
-		fmt.Println("plain content empty")
+	size := 16
+	length := len(content)
+	if len(content)%16 == 0 {
+		length = len(content)
+	} else {
+		for i := 0; i < 16; i++ {
+			length = len(content) + i
+			if length%16 == 0 {
+				break
+			}
+		}
 	}
-	ecb := cipher.NewCBCEncrypter(block, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
-	content := []byte(src)
+
 	content = PKCS5Padding(content, block.BlockSize())
 	crypted := make([]byte, len(content))
-	ecb.CryptBlocks(crypted, content)
+	for bs, be := 0, size; bs < length; bs, be = bs+size, be+size {
+		block.Encrypt(crypted[bs:be], content[bs:be])
+	}
 
 	return crypted
 }
--- main.go
+++ main.go
@@ -1,110 +1,253 @@
 package main
 
 import (
-	"bufio"
+	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/binary"
-	"encoding/hex"
 	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/davecgh/go-spew/spew"
 	"hash/crc32"
+	"io/ioutil"
 	"log"
-	"os"
-	"regexp"
+	"net"
 	"strconv"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
-func main() {
-	file := flag.String("file", "", "File to parse")
-	password := flag.String("password", "", "Password of the primary key")
+var (
+	certFile   = flag.String("cert", "someCertFile", "A PEM eoncoded certificate file.")
+	keyFile    = flag.String("key", "someKeyFile", "A PEM encoded private key file.")
+	caFile     = flag.String("CA", "someCertCAFile", "A PEM eoncoded CA's certificate file.")
+	username   = flag.String("username", "", "Homemate username")
+	password   = flag.String("password", "", "Homemate password")
+	primaryKey = flag.String("primarykey", "khggd54865SNJHGF", "Homemate primary key")
+	server     = flag.String("Hostname", "homemate.orvibo.com:10002", "Server address:port to connect")
+)
 
-	flag.Parse()
+type Client struct {
+	UserName  string
+	Password  string
+	Keys      map[string]string
+	Id        []byte
+	tlsClient *tls.Conn
+	ctx       context.Context
+}
 
-	var way int
-	way = 0
+type getSession struct {
+	SysVersion      string `json:"sysVersion"`
+	HardwareVersion string `json:"hardwareVersion"`
+	Language        string `json:"language"`
+	Source          string `json:"source"`
+	Identifier      string `json:"identifier"`
+	PhoneName       string `json:"phoneName"`
+	SoftwareVersion string `json:"softwareVersion"`
+}
 
-	var client string
+type GetSession struct {
+	defaultStruct
+	getSession
+}
 
-	var m map[string]string
-	m = make(map[string]string)
-	m["pk"] = *password
+type login struct {
+	UserName string  `json:"userName"`
+	Password string  `json:"password"`
+	FamilyID string  `json:"familyId"`
+	Type     float64 `json:"type"`
+}
 
-	dat, err := os.Open(*file)
+type executeScene struct {
+	SceneNo     string  `json:"sceneNo"`
+	UserName    string  `json:"userName"`
+	TriggerType float64 `json:"triggerType"`
+}
 
-	defer dat.Close()
-	if err != nil {
-		fmt.Print("error")
+type defaultStruct struct {
+	Cmd       float64 `json:"cmd"`
+	Serial    float64 `json:"serial"`
+	Ver       string  `json:"ver"`
+	DebugInfo string  `json:"debugInfo"`
+}
+
+type Login struct {
+	defaultStruct
+	login
+}
+
+// NewClient constructor
+func NewClient(context context.Context, username string, password string, primaryKey string) *Client {
+
+	d := &Client{
+		UserName: username,
+		Password: password,
+		ctx:      context,
 	}
-	var previousData []byte
-	r, _ := regexp.Compile("(^\\s*[0-9a-f]+$)")
-	space, _ := regexp.Compile("\\s+")
-	scanner := bufio.NewScanner(dat)
-	var merge bool
+	d.Keys = make(map[string]string)
+	d.Keys["pk"] = primaryKey
+	d.Id = []byte{32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32}
 
-	for scanner.Scan() {
-		if r.MatchString(scanner.Text()) {
-			var jsonResult map[string]interface{}
-			result := space.ReplaceAllString(scanner.Text(), "")
-			data, _ := hex.DecodeString(result)
-			if merge {
-				data = append(previousData, data...)
-			}
-			if Even(way) {
-				client = "Client"
-			} else {
-				client = "server"
-			}
+	// d.tlsClient = tlsConnection()
+	return d
+}
 
-			calcrc := crc32.ChecksumIEEE(data[42:])
-			crc := binary.BigEndian.Uint32(data[6:10])
+func (c *Client) GetSession() {
+	session := &GetSession{
+		defaultStruct{
+			Serial:    float64(7.38353548e+08),
+			Cmd:       float64(0),
+			Ver:       "4.2.3.300",
+			DebugInfo: "Android_ZhiJia365_27_4.2.3.300",
+		},
+		getSession{
+			SysVersion:      "Android8.1.0_27",
+			Source:          "ZhiJia365",
+			Identifier:      "ffffffff-8e69-cf94-0000-00007b1dd5c8",
+			Language:        "fr",
+			PhoneName:       "Nexus 5X",
+			SoftwareVersion: "40203300",
+			HardwareVersion: "LGE Nexus 5X",
+		},
+	}
+	var payload []byte
+	payload, err := json.Marshal(session)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	c.Encode(payload)
+}
 
-			if crc == calcrc {
-				merge = false
-				fmt.Print("CRC OK :" + strconv.Itoa(int(crc)) + "\n")
-				way++
-			} else {
-				// Need to append next packet
-				merge = true
-				previousData = data
-				fmt.Print("CRC Error" + "\n")
-				continue
-			}
+func (c *Client) Login() {
+	log := &Login{
+		defaultStruct{
+			Serial:    float64(6.3553185e+07),
+			Cmd:       float64(2),
+			Ver:       "4.2.3.300",
+			DebugInfo: "Android_ZhiJia365_27_4.2.3.300",
+		},
+		login{
+			UserName: c.UserName,
+			Password: c.Password,
+			FamilyID: "49ed7e5b91dd45c8af8cb37d3e4e1234",
+			Type:     float64(4),
+		},
+	}
+	var payload []byte
+	payload, err := json.Marshal(log)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	c.Encode(payload)
+}
 
-			fmt.Print("Client : " + client + "\n")
-			fmt.Print("Magic : " + string(data[0:2]) + "\n")
-			length := binary.BigEndian.Uint16(data[2:4])
-			fmt.Print("Length : " + strconv.Itoa(int(length)) + "\n")
-			fmt.Print("Type : " + string(data[4:6]) + "\n")
-			fmt.Print("Id : " + string(data[10:42]) + "\n")
-			fmt.Print("Payload Type : " + string(data[4]) + "\n")
+func (c *Client) returnKey() string {
+	if val, ok := c.Keys["dk"]; ok {
+		return val
+	}
+	return c.Keys["pk"]
+}
 
-			decrypted := AESDecrypt(data[42:], []byte(m[string(data[4:6])]))
-			json.Unmarshal(decrypted, &jsonResult)
+func (c *Client) Encode(payload []byte) {
+	encrypted := AESEncrypt(payload, []byte(c.returnKey()))
+	c.Header(encrypted)
+}
 
-			// Update Dynamic Key
-			for k, v := range jsonResult {
-				switch k {
-				case "key":
-					m["dk"] = v.(string)
-				default:
+func (c *Client) Header(payload []byte) {
+	Packet := make([]byte, len(payload)+42)
+	copy(Packet[0:2], []byte("hd"))
+	binary.BigEndian.PutUint16(Packet[2:4], uint16(len(payload)+42))
+	copy(Packet[4:6], []byte("dk"))
+	calcrc := crc32.ChecksumIEEE(payload)
+	binary.BigEndian.PutUint32(Packet[6:10], calcrc)
+	copy(Packet[10:42], c.Id)
+	copy(Packet[42:], payload)
 
-				}
-			}
-			spew.Dump(jsonResult)
+	_, err := c.tlsClient.Write(Packet)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+
+}
+
+func (c *Client) Decode(data []byte) {
+	var jsonResult map[string]interface{}
+	// var merge bool
+	// if merge {
+	// 	data = append(previousData, data...)
+	// }
+
+	calcrc := crc32.ChecksumIEEE(data[42:])
+	crc := binary.BigEndian.Uint32(data[6:10])
+
+	if crc == calcrc {
+		// merge = false
+		fmt.Print("CRC OK :" + strconv.Itoa(int(crc)) + "\n")
+		// way++
+	} else {
+		// merge = true
+		// previousData = data
+		fmt.Print("CRC Error" + "\n")
+		// continue
+	}
+
+	fmt.Print("Magic : " + string(data[0:2]) + "\n")
+	length := binary.BigEndian.Uint16(data[2:4])
+	fmt.Print("Length : " + strconv.Itoa(int(length)) + "\n")
+	fmt.Print("Type : " + string(data[4:6]) + "\n")
+	fmt.Print("Id : " + string(data[10:42]) + "\n")
+	c.Id = data[10:42]
+	fmt.Print("Payload Type : " + string(data[4]) + "\n")
+	spew.Dump([]byte(c.Keys[string(data[4:6])]))
+	decrypted := AESDecrypt(data[42:], []byte(c.Keys[string(data[4:6])]))
+	json.Unmarshal(decrypted, &jsonResult)
+
+	// Update Dynamic Key
+	for k, v := range jsonResult {
+		switch k {
+		case "key":
+			c.Keys["dk"] = v.(string)
+		default:
 		}
 	}
+	spew.Dump(jsonResult)
+}
 
-	if err := scanner.Err(); err != nil {
+func tlsConnection() *tls.Conn {
+	// Load client cert
+	cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
+	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Load CA cert
+	caCert, err := ioutil.ReadFile(*caFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Setup HTTPS client
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      caCertPool,
+	}
+	tlsConfig.BuildNameToCertificate()
+
+	conn, err := net.Dial("tcp", *server)
+	if err != nil {
+		fmt.Print("Error " + err.Error())
+	}
+
+	return tls.Client(conn, tlsConfig)
 }
 
-func Even(number int) bool {
-	return number%2 == 0
-}
+func main() {
 
-func Odd(number int) bool {
-	return !Even(number)
+	flag.Parse()
+	var ctx = context.Background()
+
+	client := NewClient(ctx, *username, *password, *primaryKey)
+	client.GetSession()
 }
