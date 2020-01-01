81f742a5aec8bb750bfe2d8dcd5c05a2592a28a5
Durand Fabrice <oeufdure@gmail.com>
2020-01-01 01:02:05 -0500

    Scene list

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net"
 	"strconv"
 	"strings"
@@ -30,18 +31,24 @@ var (
 	password   = flag.String("password", "", "Homemate password")
 	primaryKey = flag.String("primarykey", "khggd54865SNJHGF", "Homemate primary key")
 	server     = flag.String("Hostname", "homemate.orvibo.com:10002", "Server address:port to connect")
+	scenelist  = flag.Bool("scenelist", false, "Display scenes")
+	scene      = flag.String("sceneID", "", "Scene Id to trigger")
+	debug      = flag.Bool("debug", false, "Display debug")
 )
 
 // Client struct definition
 type Client struct {
-	UserName  string
-	Password  string
-	Keys      map[string]string
-	ID        []byte
-	Hashmd5   string
-	UserID    string
-	tlsClient *tls.Conn
-	ctx       context.Context
+	UserName    string
+	Password    string
+	Keys        map[string]string
+	ID          []byte
+	Hashmd5     string
+	UserID      string
+	FamilyList  map[string]interface{}
+	SiteDetails map[string]interface{}
+	FamilyID    string
+	tlsClient   *tls.Conn
+	ctx         context.Context
 }
 
 type getSession struct {
@@ -63,7 +70,6 @@ type GetSession struct {
 type login struct {
 	UserName string  `json:"userName"`
 	Password string  `json:"password"`
-	FamilyID string  `json:"familyId"`
 	Type     float64 `json:"type"`
 }
 
@@ -83,6 +89,35 @@ type executeScene struct {
 type ExecuteScene struct {
 	defaultStruct
 	executeScene
+}
+
+type userDetails struct {
+	UserID string  `json:"userId"`
+	Type   float64 `json:"type"`
+	Start  float64 `json:"start"`
+	Limit  float64 `json:"limit"`
+}
+
+// UserDetails global struct
+type UserDetails struct {
+	defaultStruct
+	userDetails
+}
+
+type homeDetails struct {
+	DeviceFlag     float64 `json:"deviceFlag"`
+	UserName       string  `json:"userName"`
+	UserID         string  `json:"userId"`
+	FamilyID       string  `json:"familyId"`
+	LastUpdateTime float64 `json:"lastUpdateTime"`
+	PageIndex      float64 `json:"pageIndex"`
+	DataType       string  `json:"dataType"`
+}
+
+// HomeDetails global struct
+type HomeDetails struct {
+	defaultStruct
+	homeDetails
 }
 
 type defaultStruct struct {
@@ -114,7 +149,7 @@ func NewClient(context context.Context, username string, password string, primar
 }
 
 // ExecuteScene method to execute a scene
-func (c *Client) ExecuteScene() {
+func (c *Client) ExecuteScene(sceneID string) {
 	scene := &ExecuteScene{
 		defaultStruct{
 			Serial:    float64(5.86385658e+08),
@@ -123,25 +158,19 @@ func (c *Client) ExecuteScene() {
 			DebugInfo: "Android_ZhiJia365_27_4.2.3.300",
 		},
 		executeScene{
-			SceneNo:     "55d15b93f1c14898beab783de1b8af89",
+			SceneNo:     sceneID,
 			TriggerType: float64(0),
 			UserName:    c.UserName,
 		},
 	}
-	var payload []byte
-	payload, err := json.Marshal(scene)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
-
-	c.Encode(payload)
+	c.marshal(scene)
 }
 
 // GetSession method to retreive the dynamic key
 func (c *Client) GetSession() {
 	session := &GetSession{
 		defaultStruct{
-			Serial:    float64(7.38353548e+08),
+			Serial:    randFloats(float64(1e+07), float64(9e+08)),
 			Cmd:       float64(0),
 			Ver:       "4.2.3.300",
 			DebugInfo: "Android_ZhiJia365_27_4.2.3.300",
@@ -156,20 +185,31 @@ func (c *Client) GetSession() {
 			HardwareVersion: "LGE Nexus 5X",
 		},
 	}
-	var payload []byte
-	payload, err := json.Marshal(session)
-	if err != nil {
-		fmt.Print(err.Error())
-	}
 
-	c.Encode(payload)
+	c.marshal(session)
+}
+
+func randFloats(min, max float64) float64 {
+	rand.Seed(time.Now().UnixNano())
+	res := min + rand.Float64()*(max-min)
+	Int64 := int(res)
+	Number := []byte(strconv.Itoa(Int64))
+	Combined := make([]byte, len(Number)+5)
+	copy(Combined[0:1], Number[0:1])
+	copy(Combined[1:2], []byte("."))
+	copy(Combined[2:], Number[1:])
+	copy(Combined[len(Number)+1:], []byte("e+0"+strconv.Itoa(len(Number)-1)))
+
+	f, _ := strconv.ParseFloat(string(Combined), 64)
+	return f
 }
 
 // Login method
 func (c *Client) Login() {
 	log := &Login{
 		defaultStruct{
-			Serial:    float64(6.3553185e+07),
+			Serial: randFloats(float64(1e+07), float64(9e+08)),
+			// Serial:    float64(6.3553185e+07),
 			Cmd:       float64(2),
 			Ver:       "4.2.3.300",
 			DebugInfo: "Android_ZhiJia365_27_4.2.3.300",
@@ -177,12 +217,109 @@ func (c *Client) Login() {
 		login{
 			UserName: c.UserName,
 			Password: c.Hashmd5,
-			FamilyID: "49ed7e5b91dd45c8af8cb37d3e4e1234",
 			Type:     float64(4),
 		},
 	}
+	c.marshal(log)
+}
+
+// UserDetails method to fetch the user details
+func (c *Client) UserDetails() {
+	user := &UserDetails{
+		defaultStruct{
+			Serial:    randFloats(float64(1e+07), float64(9e+08)),
+			Cmd:       float64(201),
+			Ver:       "4.2.3.300",
+			DebugInfo: "Android_ZhiJia365_27_4.2.3.300",
+		},
+		userDetails{
+			UserID: c.UserID,
+			Type:   float64(0),
+			Start:  float64(0),
+			Limit:  float64(20),
+		},
+	}
+	c.marshal(user)
+}
+
+// HomeDetails method to fetch the user details
+func (c *Client) HomeDetails() {
+	home := &HomeDetails{
+		defaultStruct{
+			Serial:    randFloats(float64(1e+07), float64(9e+08)),
+			Cmd:       float64(147),
+			Ver:       "4.2.3.300",
+			DebugInfo: "Android_ZhiJia365_27_4.2.3.300",
+		},
+		homeDetails{
+			UserID:         c.UserID,
+			UserName:       c.UserName,
+			FamilyID:       c.getFamilyID(),
+			PageIndex:      float64(0),
+			DeviceFlag:     float64(0),
+			DataType:       "all",
+			LastUpdateTime: float64(0),
+		},
+	}
+	c.marshal(home)
+}
+
+// getFamilyID retreive the familyId attribute
+func (c *Client) getFamilyID() string {
+	if len(c.FamilyID) > 0 {
+		return c.FamilyID
+	}
+
+	for k, v := range c.FamilyList {
+		switch k {
+		case "familyList":
+			for _, w := range v.([]interface{}) {
+				for m, x := range w.(map[string]interface{}) {
+					switch m {
+					case "familyId":
+						c.FamilyID = x.(string)
+
+					default:
+					}
+				}
+
+			}
+		default:
+		}
+	}
+	return c.FamilyID
+}
+
+// getScenes retreive all the scenes
+func (c *Client) getScenes() map[string]string {
+	var scenes map[string]string
+	scenes = make(map[string]string)
+	var sceneNo string
+	var sceneName string
+	for k, v := range c.SiteDetails {
+		switch k {
+		case "scene":
+			for _, w := range v.([]interface{}) {
+				for m, x := range w.(map[string]interface{}) {
+					switch m {
+					case "sceneName":
+						sceneName = x.(string)
+					case "sceneNo":
+						sceneNo = x.(string)
+					default:
+					}
+				}
+				scenes[sceneNo] = sceneName
+			}
+		default:
+		}
+	}
+	return scenes
+}
+
+func (c *Client) marshal(object interface{}) {
 	var payload []byte
-	payload, err := json.Marshal(log)
+	payload, err := json.Marshal(object)
 	if err != nil {
 		fmt.Print(err.Error())
 	}
@@ -236,7 +373,6 @@ func (c *Client) Send(data []byte) {
 	tmpbuf := make([]byte, 512)
 	for {
 		n, err = c.tlsClient.Read(tmpbuf)
-
 		if err != nil {
 			if err != io.EOF {
 				fmt.Println("read error:", err)
@@ -246,16 +382,15 @@ func (c *Client) Send(data []byte) {
 		if n > 0 {
 			if first {
 				length := binary.BigEndian.Uint16(tmpbuf[2:4])
-				buf = make([]byte, 0, length) // big buffer
+				buf = make([]byte, 0, length)
 				first = false
 			}
 			buf = append(buf, tmpbuf[:n]...)
 		}
-		if n <= 512 {
+		if n < 512 {
 			break
 		}
 	}
-
 	c.Decode(buf)
 }
 
@@ -266,33 +401,46 @@ func (c *Client) Decode(data []byte) {
 	calcrc := crc32.ChecksumIEEE(data[42:])
 	crc := binary.BigEndian.Uint32(data[6:10])
 
-	if crc == calcrc {
-		fmt.Print("CRC OK :" + strconv.Itoa(int(crc)) + "\n")
-	} else {
-		fmt.Print("CRC Error" + "\n")
+	length := binary.BigEndian.Uint16(data[2:4])
+
+	if *debug {
+		if crc == calcrc {
+			fmt.Print("CRC OK :" + strconv.Itoa(int(crc)) + "\n")
+		} else {
+			fmt.Print("CRC Error" + "\n")
+		}
+		fmt.Print("Magic : " + string(data[0:2]) + "\n")
+		fmt.Print("Length : " + strconv.Itoa(int(length)) + "\n")
+		fmt.Print("Type : " + string(data[4:6]) + "\n")
+		fmt.Print("Id : " + string(data[10:42]) + "\n")
 	}
 
-	fmt.Print("Magic : " + string(data[0:2]) + "\n")
-	length := binary.BigEndian.Uint16(data[2:4])
-	fmt.Print("Length : " + strconv.Itoa(int(length)) + "\n")
-	fmt.Print("Type : " + string(data[4:6]) + "\n")
-	fmt.Print("Id : " + string(data[10:42]) + "\n")
 	c.ID = data[10:42]
 
 	decrypted := AESDecrypt(data[42:], []byte(c.Keys[string(data[4:6])]))
 	json.Unmarshal(decrypted, &jsonResult)
 
-	// Update Dynamic Key
 	for k, v := range jsonResult {
 		switch k {
 		case "key":
 			c.Keys["dk"] = v.(string)
 		case "userId":
 			c.UserID = v.(string)
+		case "cmd":
+			switch v.(float64) {
+			case 201:
+				c.FamilyList = jsonResult
+			case 147:
+				c.SiteDetails = jsonResult
+			default:
+			}
+
 		default:
 		}
 	}
-	spew.Dump(jsonResult)
+	if *debug {
+		spew.Dump(jsonResult)
+	}
 }
 
 func tlsConnection() *tls.Conn {
@@ -309,12 +457,12 @@ func tlsConnection() *tls.Conn {
 	}
 	caCertPool := x509.NewCertPool()
 	caCertPool.AppendCertsFromPEM(caCert)
-
+	serverName := strings.Split(*server, ":")
 	// Setup HTTPS client
 	tlsConfig := &tls.Config{
 		Certificates: []tls.Certificate{cert},
 		RootCAs:      caCertPool,
-		ServerName:   "homemate.orvibo.com",
+		ServerName:   serverName[0],
 	}
 	tlsConfig.BuildNameToCertificate()
 
@@ -336,5 +484,12 @@ func main() {
 	client := NewClient(ctx, *username, *password, *primaryKey)
 	client.GetSession()
 	client.Login()
-	client.ExecuteScene()
+	client.UserDetails()
+	client.HomeDetails()
+	if *scenelist {
+		spew.Dump(client.getScenes())
+	}
+	if len(*scene) > 0 {
+		client.ExecuteScene(*scene)
+	}
 }
